9144997451f85aecd59faa4bf22651401cd3cbb9
Bruno Francisco <64901068+brunofranciscu@users.noreply.github.com>
2024-07-05 11:45:59 -0300

    redirect test

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Menu from "../pages/Menu/index.tsx";
 import Contact from "../pages/Contact/index.tsx";
 import Header from "../Header.tsx";
+import { Fragment } from "react";
 
-export default function Rotas(){
-    return(
-    <BrowserRouter>
-        <Header />
-        <Routes>
-            <Route path="/" element={<Menu/>}/>
-            <Route index path="/Menu" element={<Menu/>}/>
-            <Route path="/Contact" element={<Contact/>}/>
-        </Routes>
-    </BrowserRouter>
-    )
+export default function Rotas() {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Fragment>
+                <Routes>
+                    <Route path="/Menu" element={<Menu />} />
+                    <Route path="/" element={<Menu />} />
+                    <Route path="/Contact" element={<Contact />} />
+                    <Route path="*" element={<Menu />} />
+                </Routes>
+            </Fragment>
+        </BrowserRouter>
+    );
 }
